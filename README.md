a6061f30ecaca354c4bdb9bc53c82df30aba2336
markvelous <85272691+markvelous@users.noreply.github.com>
2021-07-02 13:39:52 +0800

    Markobot botdapp2

---
+++ README.md
@@ -0,0 +1,15 @@
+# Markobot botapp2
+
+P7210027 Mark Tan SGBT4 Blockchain Beginner Course Project
+
+Creating an app that allows any user to log in through Metamask to mint an NFT
+
+### npm start 
+
+Access the app on http://localhost:3000
+Connect Metamask wallet
+
+Enter location of digital asset to be minted as ERC721
+Enter name and metadata for the NFT, and mint
+
+## The app runs on Rinkeby and the IPFS is published on Pinata 
---
+++ src/Minter.js
@@ -0,0 +1,114 @@
+import { useEffect, useState } from "react";
+import {
+  connectWallet,
+  getCurrentWalletConnected,
+  mintNFT,
+} from "./util/interact.js";
+
+const Minter = (props) => {
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [url, setURL] = useState("");
+
+  useEffect(async () => {
+    const { address, status } = await getCurrentWalletConnected();
+
+    setWallet(address);
+    setStatus(status);
+
+    addWalletListener();
+  }, []);
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Create a story and give life to the NFT!");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
+  };
+
+  const onMintPressed = async () => {
+    const { success, status } = await mintNFT(url, name, description);
+    setStatus(status);
+    if (success) {
+      setName("");
+      setDescription("");
+      setURL("");
+    }
+  };
+
+  return (
+    <div className="Minter">
+      <button id="walletButton" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect 🦊 Wallet</span>
+        )}
+      </button>
+
+      <br></br>
+      <h1 id="title">Marköbot 🤖 NFT Minter</h1>
+      <p>
+        Add your digital asset's link, name and description, <br></br>then press "Mint my Unique Marköbot" to create your NFT
+      </p>
+      <form>
+        <h2>Location of the digital asset (IPFS): </h2>
+        <input
+          type="text"
+          placeholder="Where it is located, e.g. https://gateway.pinata.cloud/ipfs/<hash>"
+          onChange={(event) => setURL(event.target.value)}
+        />
+        <h2>Name of the NFT: </h2>
+        <input
+          type="text"
+          placeholder="e.g. My first NFT!"
+          onChange={(event) => setName(event.target.value)}
+        />
+        <h2>Metadata of the NFT: </h2>
+        <input
+          type="text"
+          placeholder="e.g. Now to sell it to a whale ;)"
+          onChange={(event) => setDescription(event.target.value)}
+        />
+      </form>
+      <button id="mintButton" onClick={onMintPressed}>
+      🔥 Mint my unique Marköbot
+      </button>
+      <p id="status">
+        {status}
+      </p>
+    </div>
+  );
+};
+
+export default Minter;
